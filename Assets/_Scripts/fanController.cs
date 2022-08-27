6414a782e32cd5d15c97430bcf59d6317c1b6ed2
equintee <69682804+equintee@users.noreply.github.com>
2022-08-27 23:13:19 +0300

    EU

--- Assets/_Scripts/fanController.cs
+++ Assets/_Scripts/fanController.cs
@@ -12,8 +12,8 @@ public class fanController : MonoBehaviour
     private float touchSensivity;
     private Vector3 blowPower;
     private float movementSpeed;
-    private int raycastLayer;
-    private int obsticleLayer;
+    private int playerLayer;
+    private int fanLayer;
     private PathCreator pathCreator;
     private float distanceTravelled = 0f;
     private float initialHeight;
@@ -27,8 +27,8 @@ public class fanController : MonoBehaviour
         touchSensivity = levelController.touchSensivity;
         blowPower = levelController.blowPower;
         movementSpeed = levelController.movementSpeed;
-        raycastLayer = LayerMask.GetMask("Default", "Fan", "obsticle");
-        obsticleLayer = LayerMask.NameToLayer("obsticle");
+        playerLayer = LayerMask.GetMask("Platform");
+        fanLayer = LayerMask.GetMask("Fan");
         pathCreator = FindObjectOfType<PathCreator>();
     }
     private void FixedUpdate()
@@ -44,8 +44,19 @@ public class fanController : MonoBehaviour
         Vector3 destination = pathCreator.path.GetPointAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
         destination.y = initialHeight;
 
+        ballRigidBody.velocity = Vector3.Scale(ballRigidBody.velocity, transform.right) + new Vector3(0, ballRigidBody.velocity.y, 0);
+
         transform.position = destination;
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
+
+        if (ballRigidBody.velocity.y > 0 && ballRigidBody.position.y > initialHeight + 2)
+        {
+            Vector3 velocity = ballRigidBody.velocity;
+            velocity.y *= 0.5f;
+            ballRigidBody.velocity = velocity;
+        }
+            
+            
     }
 
     private void fanMovement()
@@ -56,8 +67,8 @@ public class fanController : MonoBehaviour
             if (touch.phase == TouchPhase.Moved)
             {
                 Vector3 movement = new Vector3(0, touch.deltaPosition.x * Time.fixedDeltaTime * touchSensivity * -1, 0);
-                gunModel.transform.Translate(movement);
-                
+                if(Physics.Raycast(gunModel.transform.position, Vector3.down, 3f, playerLayer))
+                    gunModel.transform.localPosition += movement;
             }
         }
     }
@@ -73,52 +84,27 @@ public class fanController : MonoBehaviour
             velocity.y -= 5 * Time.fixedDeltaTime;
             ballRigidBody.velocity = velocity;
             return;
-
         }
         
-        if (Physics.SphereCast(ballRigidBody.position, 1, Vector3.down, out RaycastHit hit, 5f, raycastLayer))
+        if (Physics.SphereCast(ballRigidBody.position, 1, Vector3.down, out RaycastHit hit, 2.5f, fanLayer))
         {
-            //engel??????????????????
-            if (hit.transform.gameObject.layer == obsticleLayer)
-                return;
+            if(ballRigidBody.velocity.y < 0)
+            {
+                Vector3 ballVelocity = ballRigidBody.velocity;
+                ballVelocity.y *= 0.5f;
+                ballRigidBody.velocity = ballVelocity;
+            }
 
-            Vector3 velocity = calculateVelocity();
-            ballRigidBody.AddForceAtPosition(velocity * 3, transform.position);
+            Vector3 force = calculateForce();
+            ballRigidBody.AddForceAtPosition(force, gunModel.transform.position);
         }
     }
 
-    /*private Vector3 calculateVelocity()
+    private Vector3 calculateForce()
     {
-        Vector3 velocity = Vector3.Normalize(ballRigidBody.position - transform.position);
-        velocity.z = 0;
-        velocity.x *= blowPower.x;
-        velocity.y *= blowPower.y;
-
-        if (ballRigidBody.velocity.y < 0)
-            velocity.y -= ballRigidBody.velocity.y;
-
-        if ((transform.position.x < ballRigidBody.position.x && velocity.x < 0) || (transform.position.x > ballRigidBody.position.x && velocity.x > 0))
-            velocity.x *= 2;
-        
-        return velocity;
-        
-
-    }*/
-
-    private Vector3 calculateVelocity()
-    {
-        Vector3 velocity = Vector3.Normalize(ballRigidBody.position - transform.position);
-        velocity.z = 0;
-        velocity.x *= blowPower.x;
-        velocity.y *= blowPower.y;
-
-        if (ballRigidBody.velocity.y < 0)
-            velocity.y -= ballRigidBody.velocity.y;
-
-        if ((ballRigidBody.position.x > 0 && transform.position.x > 0) || (ballRigidBody.position.x < 0 && transform.position.x < 0))
-            velocity.x *= 2;
-
-        return velocity;
-
+        Vector3 force = Vector3.one;
+        Vector3 horizantalForce = Vector3.Normalize(ballRigidBody.position - gunModel.transform.position) * blowPower.x;
+        Vector3 verticalForce = new Vector3(0, blowPower.y, 0);
+        return horizantalForce + verticalForce;
     }
 }
